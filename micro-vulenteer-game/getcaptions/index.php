18b48562f1d26337aed23dc657ee86ec6d259c78
arno koehler <arno.koehler@gmail.com>
2014-02-09 16:33:36 +0100

    changed to real video

--- micro-vulenteer-game/getcaptions/index.php
+++ micro-vulenteer-game/getcaptions/index.php
@@ -29,33 +29,33 @@ mysqli_close($con);
 
 
 $entry["captionid"] = 100;
-$entry["url"] = "http://www.youtube.com/embed/okqEVeNqBhc";
+$entry["url"] = "https://www.youtube.com/watch?v=kpCJyQ2usJ4";
 $entry["subject"] = "math";
-$entry["startpoint"] = "131";
-$entry["endpoint"] = "135";
+$entry["startpoint"] = "20";
+$entry["endpoint"] = "24";
 $entry["accuracy"] = 85;
 $entry["language"] = "english";
-$entry["caption"] = "I will never let you be rickrolled";
+$entry["caption"] = "but we cannot understood it if we do not first run the language";
 $data[0]['video'] = $entry;
 
 $entryTwo["captionid"] = 100;
-$entryTwo["url"] = "http://www.youtube.com/embed/okqEVeNqBhc";
+$entryTwo["url"] = "https://www.youtube.com/watch?v=kpCJyQ2usJ4";
 $entryTwo["subject"] = "math";
-$entryTwo["startpoint"] = "135";
-$entryTwo["endpoint"] = "138";
+$entryTwo["startpoint"] = "43";
+$entryTwo["endpoint"] = "46";
 $entryTwo["accuracy"] = 85;
 $entryTwo["language"] = "english";
-$entryTwo["caption"] = "Or maybe you are ...";
+$entryTwo["caption"] = "in deeper into blahblahbla is that we're going to start abh4cking things are";
 $data[1]['video'] = $entryTwo;
 
 $entryThree["captionid"] = 100;
-$entryThree["url"] = "http://www.youtube.com/embed/okqEVeNqBhc";
+$entryThree["url"] = "https://www.youtube.com/watch?v=kpCJyQ2usJ4";
 $entryThree["subject"] = "math";
-$entryThree["startpoint"] = "145";
-$entryThree["endpoint"] = "148";
-$entryThree["accuracy"] = 85;
+$entryThree["startpoint"] = "105";
+$entryThree["endpoint"] = "115";
+$entryThree["accuracy"] = 20;
 $entryThree["language"] = "english";
-$entryThree["caption"] = "You are being Rick Rolled";
+$entryThree["caption"] = "so I'm interested in a twenty dollar in my pocket I'm, I'm, I'm hunting, looking for a come up, this is fucking awesome";
 $data[2]['video'] = $entryThree;
   
 $number = rand(0,2);
