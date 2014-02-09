ea14dcf94d0c7a41fee79d3a922fbc225a26ae64
arno koehler <arno.koehler@gmail.com>
2014-02-09 15:19:11 +0100

    index.php

--- micro-vulenteer-game/getcaptions/index.php
+++ micro-vulenteer-game/getcaptions/index.php
@@ -4,4 +4,22 @@
 
 // determine user score 
 // based on that pick next level
+
+
+$con=mysqli_connect("localhost","hack4good","geeklist","hack4good");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+$sql="SELECT * FROM TABLE";
+
+if (!mysqli_query($con,$sql))
+  {
+  die('Error: ' . mysqli_error($con));
+  }
+echo "1 record added";
+
+mysqli_close($con);
 ?>
