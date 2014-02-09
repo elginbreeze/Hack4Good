a2ab1f7b9afbd8dea0bdabe86a3f746a62728773
arno koehler <arno.koehler@gmail.com>
2014-02-09 16:42:55 +0100

    layout & offset

--- micro-vulenteer-game/getcaptions/index.php
+++ micro-vulenteer-game/getcaptions/index.php
@@ -29,17 +29,17 @@ mysqli_close($con);
 
 
 $entry["captionid"] = 100;
-$entry["url"] = "https://www.youtube.com/watch?v=kpCJyQ2usJ4";
+$entry["url"] = "http://www.youtube.com/embed/kpCJyQ2usJ4";
 $entry["subject"] = "math";
 $entry["startpoint"] = "20";
-$entry["endpoint"] = "24";
+$entry["endpoint"] = "25";
 $entry["accuracy"] = 85;
 $entry["language"] = "english";
 $entry["caption"] = "but we cannot understood it if we do not first run the language";
 $data[0]['video'] = $entry;
 
 $entryTwo["captionid"] = 100;
-$entryTwo["url"] = "https://www.youtube.com/watch?v=kpCJyQ2usJ4";
+$entryTwo["url"] = "http://www.youtube.com/embed/kpCJyQ2usJ4";
 $entryTwo["subject"] = "math";
 $entryTwo["startpoint"] = "43";
 $entryTwo["endpoint"] = "46";
@@ -49,10 +49,10 @@ $entryTwo["caption"] = "in deeper into blahblahbla is that we're going to start 
 $data[1]['video'] = $entryTwo;
 
 $entryThree["captionid"] = 100;
-$entryThree["url"] = "https://www.youtube.com/watch?v=kpCJyQ2usJ4";
+$entryThree["url"] = "http://www.youtube.com/embed/kpCJyQ2usJ4";
 $entryThree["subject"] = "math";
 $entryThree["startpoint"] = "105";
-$entryThree["endpoint"] = "115";
+$entryThree["endpoint"] = "110";
 $entryThree["accuracy"] = 20;
 $entryThree["language"] = "english";
 $entryThree["caption"] = "so I'm interested in a twenty dollar in my pocket I'm, I'm, I'm hunting, looking for a come up, this is fucking awesome";
